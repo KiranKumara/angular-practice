f56583941fa74dab1103f4ed8e82dd324177be5e
Kiran Kumara <kiran.kumara@sumerusolutions.com>
2019-01-05 19:14:57 -0800

    Forms template driven approach & the reactive approach.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-practice';
+export class AppComponent implements OnInit {
+  @ViewChild('f') userForm: NgForm;
+  title = 'recipe-shopper';
+  defaultOption = 'advanced';
+  formSubmitted = false;
+  user = {
+    enteredEmail: '',
+    selectedSubscription: ''
+  };
+  projectStatuses = ['Stable', 'Critical', 'Finished'];
+  projectForm: FormGroup;
+  projectFormSubmited = false;
+
+
+  ngOnInit () {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, [Validators.required, this.forbiddenProjectName], this.forbiddenProjectNameFromServer),
+      'projectEmail': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl(null)
+    })
+  }
+
+  onSubmit() {
+    this.formSubmitted = true
+    console.log(this.userForm.value);
+    this.user.enteredEmail = this.userForm.value.email;
+    this.user.selectedSubscription = this.userForm.value.subscription;
+  }
+
+  onProjectFormSubmit() {
+    this.projectFormSubmited = this.projectForm.valid;
+  }
+
+  forbiddenProjectName(control: FormControl): {[s: string]: boolean} {
+    if (control.value == 'Test') {
+      return {'forbiddenProjectName': true};
+    }
+    return null;
+  }
+
+  forbiddenProjectNameFromServer(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value == 'TestProjectname') {
+          resolve({'forbiddenProjectName': true});
+        } else {
+          resolve(null);
+        }
+      }, 3000);
+    });
+    return promise;
+  }
 }
